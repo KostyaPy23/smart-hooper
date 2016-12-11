e63615666c71ca7b1eb05d7d3aae16d061fa807c
Kostya <negrunKostya@gmail.com>
2016-12-11 21:46:48 +0200

    add some logic

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,61 @@
-// (function($) {
-//     'use strict';
-//
-// }(jQuery));
+(function ($) {
+    'use strict';
+
+    $(document).ready(function () {
+        var answers = [];
+
+        $('#rootwizard').bootstrapWizard({
+            nextSelector: '.wizard .btn-next',
+            previousSelector: '.wizard .btn-prev',
+
+            onTabClick: function (tab, navigation, index) {
+                // alert('on tab click disabled');
+                // return false;
+            },
+
+            onNext: function(tab, navigation, index) {
+                if (clicked($(".variants .variant-item")) == false) {
+                    return false;
+                }
+            }
+        });
+
+        function clicked(element) {
+            var count = 0;
+
+            element.click(function() {
+                count++;
+                if (count > 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+        }
+
+
+        $('.variants').each(function() {
+            var $item = $(this).find('.variant-item');
+            var count = 0;
+
+            $item.click(function() {
+                var $variantText = $(this).find(".variant-caption");
+                var $self = $(this);
+                count++;
+
+                if (count == 1) {
+                    $self.addClass('selected');
+
+                    if ($self.hasClass('selected')) {
+                        answers.push($.trim($variantText.html()));
+                        console.log(answers);
+                        return true;
+                    }
+                }
+                return false;
+            });
+        });
+
+    });
+
+}(jQuery));
